a794828c78ed92ff55cd492e6e97b5d40a3bac50
HenriqueFM96 <henriquefm96@hotmail.com>
2024-08-08 09:52:33 -0300

    fix internal load balancer IP association

--- 01-azure-security-hub.tf
+++ 01-azure-security-hub.tf
@@ -101,6 +101,7 @@ resource "azurerm_lb" "internal-lb" {
     name = "internal-frontend-ip"
     subnet_id = azurerm_subnet.azure-hub-trusted.id
     private_ip_address_allocation = "Static"
+    private_ip_address = "10.0.10.4"
   }
 }
 

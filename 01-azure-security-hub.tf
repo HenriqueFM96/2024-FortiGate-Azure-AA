daee3d2ef7bb7d43764ddf34bee267b024303898
HenriqueFM96 <henriquefm96@hotmail.com>
2024-08-08 09:46:16 -0300

    fix load balancer PIP association

--- 01-azure-security-hub.tf
+++ 01-azure-security-hub.tf
@@ -80,7 +80,7 @@ resource "azurerm_lb" "fgt-external-lb" {
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.fgt-lb-pip.ip_address
+    public_ip_address_id = azurerm_public_ip.fgt-lb-pip.id
   }
 }
 

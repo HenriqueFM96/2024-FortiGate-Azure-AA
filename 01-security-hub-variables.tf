79ef510d865da36d674b4c816f94ed8673e81cd1
HenriqueFM96 <henriquefm96@hotmail.com>
2024-08-08 21:35:19 -0300

    adding FortiGate-VMs to the deployment script

--- 01-security-hub-variables.tf
+++ 01-security-hub-variables.tf
@@ -55,12 +55,6 @@ variable "custom_image_resource_group_name" {
   default = "<custom image resource group>"
 }
 
-// License Type to create FortiGate-VM
-// Provide the license type for FortiGate-VM Instances, either byol or payg.
-variable "license_type" {
-  default = "payg"
-}
-
 variable "publisher" {
   type    = string
   default = "fortinet"
@@ -133,15 +127,81 @@ variable "hub-fgt_B-internal-ip-address" {
   default = "10.0.10.11"
 }
 
-variable "bootstrap-fgtvm" {
-  // Change to your own path
+variable "bootstrap-fgt-vm-A" {
   type    = string
-  default = "fortigate.conf"
+  default = "01-fortigate-vm-A.conf"
+}
+
+variable "bootstrap-fgt-vm-B" {
+  type    = string
+  default = "01-fortigate-vm-B.conf"
+}
+
+variable "license_type" {
+  default = "byol"
+}
+
+variable "publisher" {
+  type    = string
+  default = "fortinet"
+}
+
+variable "fgtoffer" {
+  type    = string
+  default = "fortinet_fortigate-vm_v5"
+}
+
+// BYOL sku: fortinet_fg-vm
+// PAYG sku: fortinet_fg-vm_payg_2022
+variable "fgtsku" {
+  type = map(any)
+  default = {
+    byol = "fortinet_fg-vm"
+    payg = "fortinet_fg-vm_payg_2023"
+  }
 }
 
 // license file for the fgt
-variable "license" {
+variable "license-MemberA" {
   // Change to your own byol license file, license.lic
   type    = string
-  default = "license.txt"
+  default = "FGT-VM-MemberA.txt"
+}
+
+variable "license-MemberB" {
+  // Change to your own byol license file, license.lic
+  type    = string
+  default = "FGT-VM-MemberB.txt"
+}
+
+variable "fgtversion" {
+  type    = string
+  default = "7.6.0"
+}
+
+variable "fortigate-vm-size" {
+  type = string
+  default = "Standard_B1s"
+}
+
+// To use custom image
+// by default is false
+variable "custom" {
+  default = false
+}
+
+//  Custom image blob uri
+variable "customuri" {
+  type    = string
+  default = "<custom image blob uri>"
+}
+
+variable "custom_image_name" {
+  type    = string
+  default = "<custom image name>"
+}
+
+variable "custom_image_resource_group_name" {
+  type    = string
+  default = "<custom image resource group>"
 }
